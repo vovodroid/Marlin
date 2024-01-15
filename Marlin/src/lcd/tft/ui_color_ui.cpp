955a53882050de400149ead562e643a84ecb3e0d
vovodroid <vovodroid@users.noreply.github.com>
2024-01-15 16:34:26 +0200

    Enable SD icon

--- Marlin/src/lcd/tft/ui_color_ui.cpp
+++ Marlin/src/lcd/tft/ui_color_ui.cpp
@@ -324,7 +324,7 @@ void MarlinUI::draw_status_screen() {
       if (cm && pa)
         add_control(SDCARD_ICON_X, SDCARD_ICON_Y, STOP, imgCancel, true, COLOR_CONTROL_CANCEL);
       else
-        add_control(SDCARD_ICON_X, SDCARD_ICON_Y, menu_media, imgSD, cm && !pa, COLOR_CONTROL_ENABLED, COLOR_CONTROL_DISABLED);
+        add_control(SDCARD_ICON_X, SDCARD_ICON_Y, menu_media, imgSD, /*cm &&*/ !pa, COLOR_CONTROL_ENABLED, COLOR_CONTROL_DISABLED);
     #endif
   #endif
 
