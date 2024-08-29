14172072b2c2f9fe1e0513d8c513cf9ecf4edcf0
vovodroid <vovodroid@users.noreply.github.com>
2024-08-29 13:51:33 +0300

    G28 F

--- Marlin/src/module/motion.h
+++ Marlin/src/module/motion.h
@@ -79,7 +79,7 @@ extern xyz_pos_t cartes;
  * Feed rates are often configured with mm/m
  * but the planner and stepper like mm/s units.
  */
-constexpr xyz_feedrate_t homing_feedrate_mm_m = HOMING_FEEDRATE_MM_M;
+extern xyz_feedrate_t homing_feedrate_mm_m;
 FORCE_INLINE feedRate_t homing_feedrate(const AxisEnum a) {
   float v = TERN0(HAS_Z_AXIS, homing_feedrate_mm_m.z);
   #if DISABLED(DELTA)
